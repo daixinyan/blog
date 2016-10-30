0c72fee7cdc3fa9299188c5d0ca95598d57ec42f
dxy14 <dxy14@software.nju.edu.cn>
2016-10-30 18:19:29 +0800

    change database.

--- core/config/database.php
+++ core/config/database.php
@@ -6,10 +6,14 @@
  * Time: 14:54
  */
 return [
-    'SEVER'=>'123.206.202.103',
+//    'SEVER'=>'123.206.202.103',
+//    'DATABASE'=>'blog',
+//    'USERNAME'=>'user',
+//    'PASSWORD'=>'root',
+    'SEVER'=>'115.29.138.254',
     'DATABASE'=>'blog',
-    'USERNAME'=>'user',
-    'PASSWORD'=>'root',
+    'USERNAME'=>'blog',
+    'PASSWORD'=>'083416',
 ];
 
 
