4fa1e7b8a4991e921b9744291cd50fa62cd49f6f
daixinyan <381675152@qq.com>
2016-12-19 17:11:21 +0800

    add prefix config for framwork.

--- core/App.php
+++ core/App.php
@@ -81,10 +81,9 @@ class App
         self::$router = require APP_PATH.'core/config/router.php';
         self::$router['/'] = [ ['GET','POST'],[self::$controller,self::$method],[] ];
         self::$request_url = $_SERVER['REQUEST_URI'];//start with '/'
-        if(WEB_PREFIX=='' || strpos(self::$request_url, WEB_PREFIX)==0)
-        {
-            self::$request_url = substr(self::$request_url, strlen(WEB_PREFIX));
-        }
+
+        self::filterRequestURL();
+
         self::$url_array = explode('/',self::$request_url);// the first element must be ""(empty)
         self::$request_method = $_SERVER['REQUEST_METHOD'];
         self::$mapper = include APP_PATH.'core/config/mapper.php';
@@ -92,6 +91,20 @@ class App
     }
 
 
+    protected static function filterRequestURL(){
+        $useless_string = '?url=';
+        //remove prefix
+        if(WEB_PREFIX=='' || strpos(self::$request_url, WEB_PREFIX)==0)
+        {
+            self::$request_url = substr(self::$request_url, strlen(WEB_PREFIX));
+        }
+        //remove postfix
+        $exist_useless_string = strripos(self::$request_url, $useless_string);
+        if($exist_useless_string && $exist_useless_string+strlen($useless_string) == strlen(self::$request_url))
+        {
+            self::$request_url = substr(self::$request_url, 0, $exist_useless_string);
+        }
+    }
 
     
     protected static function findMatchUrl(){
